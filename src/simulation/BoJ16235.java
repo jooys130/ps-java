372afe94b843684e6684b1c3cbb2344a193b3af6
jooys130 <wndustn130@gmail.com>
2024-01-20 23:52:27 +0900

    [simulation] BOJ G3 16235 나무 재테크
    Deque 사용
    정렬하는 방법도 있음

--- src/simulation/BoJ16235.java
+++ src/simulation/BoJ16235.java
@@ -1,23 +1,31 @@
-package samsung;
+package simulation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+class Tree {
+
+    int x;
+    int y;
+    int age;
+
+    public Tree(int x, int y, int age) {
+        this.x = x;
+        this.y = y;
+        this.age = age;
+    }
+}
 
 public class BoJ16235 {
 
     static int N, M, K;
     static int[][] A;
-    static Queue<Integer> queue;
     static int[][] food;
-    static int[][] deadTreeCount;
+    static Deque<Tree> trees;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
@@ -33,25 +41,78 @@ public class BoJ16235 {
         // 추가될 양분의 양
         A = new int[N][N];
         for (int i = 0; i < N; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                A[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 구입한 M개의 나무를 땅에 심음: 나이 기반
-        for (int i = 0; i < N; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-            int y = Integer.parseInt(stringTokenizer.nextToken()) - 1;
-            int age = Integer.parseInt(stringTokenizer.nextToken());
-
+        // M개의 나무를 땅에 심음
+        trees = new LinkedList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int age = Integer.parseInt(st.nextToken());
+            trees.offer(new Tree(x, y, age));
         }
 
+        // K년 지난 후
         for (int year = 0; year < K; year++) {
-            deadTreeCount = new int[N][N];
             ss();
             fw();
+        }
+        System.out.println(trees.size());
+    }
+
+    private static void ss() {
+        List<Tree> deadTrees = new ArrayList<>();
+        Deque<Tree> next = new LinkedList<>();
+        // 봄: 나이만큼 양분 먹기
+        for (Tree tree : trees) {
+            if (food[tree.x][tree.y] < tree.age) {
+                deadTrees.add(tree);
+            } else {
+                food[tree.x][tree.y] -= tree.age;
+                next.addLast(new Tree(tree.x, tree.y, tree.age + 1));
+            }
+        }
+        // 여름: 죽은 나무를 양분으로
+        for (Tree tree : deadTrees) {
+            food[tree.x][tree.y] += tree.age / 2;
+        }
+        trees = next;
+    }
+
+    private static void fw() {
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        // 가을: 나무 번식
+        List<Tree> newTrees = new ArrayList<>();
+        for (Tree tree : trees) {
+            int x = tree.x;
+            int y = tree.y;
+            int age = tree.age;
+            if (age % 5 == 0) {
+                for (int i = 0; i < 8; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                        continue;
+                    }
+                    newTrees.add(new Tree(nx, ny, 1));
+                }
+            }
+        }
+        // 어린 나무를 앞으로
+        for (Tree tree : newTrees) {
+            trees.addFirst(tree);
+        }
+        // 겨울: 양분 추가
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                food[i][j] += A[i][j];
+            }
         }
     }
 }

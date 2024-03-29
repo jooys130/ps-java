f161544eee144e731e904436239f811f3504ca53
jooys130 <wndustn130@gmail.com>
2024-03-30 02:42:51 +0900

    N-Queen(브루트포스/백트래킹)

--- src/BOJ/Main_9663_G4_NQueen.java
+++ src/BOJ/Main_9663_G4_NQueen.java
@@ -3,54 +3,45 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main_9663_G4_NQueen {
     static int N;
-    static boolean[][] map;
+    static int[] map; // index : 행 / value : 열
     static int ans;
     public static void dfs(int x) {
         if (x == N) {
-//            for (int i = 0; i < N; i++) {
-//                System.out.println(Arrays.toString(map[i]));
-//            }
-//            System.out.println();
+             // System.out.println(Arrays.toString(map));
             ans++;
             return;
         }
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             if (!promising(x, i)) continue;
-            map[x][i] = true;
+            map[x] = i;
             dfs(x+1);
-            map[x][i] = false;
         }
     }
-    public static boolean promising(int x, int y) {
-        for (int i = 0; i < N; i++) {
-            if (map[x][i]) return false;
+    public static boolean promising(int x, int value) {
+        // 동일 열
+        for (int i = 0; i < x; i++) {
+            if (map[i] == value) return false;
         }
-        for (int i = 0; i < N; i++) {
-            if(map[i][y]) return false;
-        }
-        // 대각선 위로만 !!
-        for (int i = x-1; i >= 0; i--) {
-            for (int j = 0; j < N; j++) {
-                // 오른쪽 위
-                if (x + y == i + j && map[i][j]) return false;
-                // 왼쪽 위
-                if (x - y == (i - j) && map[i][j]) return false;
-            }
+        for (int i = 0; i < x; i++) {
+            if (Math.abs(x - i) == Math.abs(value - map[i])) return false;
+            /*
+            // 대각선 왼쪽 위
+            if (map[i] - i == value - x) return false;
+            // 대각선 오른쪽 위
+            if (map[i] + i == value + x) return false;
+             */
         }
         return true;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            map[0][i] = true;
-            dfs(1);
-            map[0][i] = false;
-        }
+        map = new int[N];
+        dfs(0);
         System.out.println(ans);
     }
 }

67941166cc6f0d61a9608164230ddd4794927d9f
jooys130 <wndustn130@gmail.com>
2024-04-25 10:32:00 +0900

    가장 큰 증가하는 부분 수열(다이나믹 프로그래밍)

---
+++ src/BOJ/Main_11055_S2_가장큰증가하는부분수열.java
@@ -0,0 +1,33 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11055_S2_가장큰증가하는부분수열 {
+    static int N;
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+        Arrays.sort(dp);
+        System.out.println(dp[N-1]);
+    }
+}
